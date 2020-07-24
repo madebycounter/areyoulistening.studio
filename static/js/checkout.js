7e78028ba281b7b90f6ba0c54a140886bf9ddece
William Gardner <wg4568@gmail.com>
2020-07-24 14:13:53 -0700

    refined checkout select behavior

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -7,17 +7,42 @@ $(() => {
     if ($('#mockup')[0].complete) $('#mockup').trigger('load')
 })
 
+var select_open = false
+var click_count = 0
 $('select').click((e) => {
     $('#default').remove()
+    select_open = !select_open
+    click_count += 1
 })
 
+$('select').on('blur', (e) => {
+    select_open = false
+    close_select()
+})
+
+$(document).keyup(function(e) {
+    if (e.keyCode == 27) {
+        select_open = false
+        close_select()
+    }
+})
+
+function close_select() {
+    if (!select_open && click_count != 0) allow_paypal()
+}
+
+
 $('select').change((e) => {
+    allow_paypal()
+})
+
+function allow_paypal() {
     $('#paypal_checkout').removeClass('hidden')
 
     setTimeout(() => {
         $('html, body').animate({ scrollTop: 200 })
     }, 100)
-})
+}
 
 paypal.Buttons({
     style: {
