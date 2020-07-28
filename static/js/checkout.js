a02418c2f097ee2e863223a44d1510b3c05bbc4a
William Gardner <wg4568@gmail.com>
2020-07-27 19:26:06 -0700

    added decimal to paypal

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -56,7 +56,7 @@ paypal.Buttons({
         return actions.order.create({
             purchase_units: [{
                 amount: {
-                    value: '24'
+                    value: '24.00'
                 }
             }]
         });
