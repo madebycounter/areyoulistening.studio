8f5ff769172116fbfcd731a252140ec77c1ed5b3
William Gardner <wg4568@gmail.com>
2020-08-04 12:40:19 -0700

    made checkout size selector more reliable

--- static/js/checkout.js
+++ static/js/checkout.js
@@ -7,34 +7,12 @@ $(() => {
     if ($('#mockup')[0].complete) $('#mockup').trigger('load')
 })
 
-var select_open = false
-var click_count = 0
-$('select').click((e) => {
-    $('#default').remove()
-    select_open = !select_open
-    click_count += 1
-    close_select()
-})
-
-$('select').on('blur', (e) => {
-    select_open = false
-    close_select()
-})
-
-$(document).keyup(function(e) {
-    if (e.keyCode == 27) {
-        select_open = false
-        close_select()
+$('#size').click(() => {
+    if (['small', 'medium', 'large', 'extralarge'].indexOf($('#size').val()) != -1) {
+        allow_paypal()
+    } else {
+        hide_paypal()
     }
-})
-
-function close_select() {
-    if (!select_open && click_count != 0) allow_paypal()
-}
-
-
-$('select').change((e) => {
-    allow_paypal()
 })
 
 function allow_paypal() {
@@ -42,6 +20,14 @@ function allow_paypal() {
 
     setTimeout(() => {
         $('html, body').animate({ scrollTop: 200 })
+    }, 100)
+}
+
+function hide_paypal() {
+    $('#paypal_checkout').addClass('hidden')
+
+    setTimeout(() => {
+        $('html, body').animate({ scrollTop: 0 })
     }, 100)
 }
 
